dee6895e974e90fafccb633bc5c3fe4e2a8ecd94
clovizx23-thetwo <soarezclovizz018@gmail.com>
2026-09-11 00:52:06 -0300

    falta implementar a fx principal

--- TapetedeSierpinski.c
+++ TapetedeSierpinski.c
@@ -6,7 +6,7 @@ void tapeteSierpinski(int i, int j, int size, int M){
 
 }
 int **criarMatriz(n){
-    int **tapMat = (int*)malloc(sizeof(int*)*n);
+    int **tapMat = (int**)malloc(sizeof(int*)*n);
     for(int j = 0; j < n; j++){
         tapmat[j] = (int*)malloc(sizeof(int)*n);
         for(int k = 0; k < n; k++){
@@ -14,4 +14,10 @@ int **criarMatriz(n){
         }
     }
     return tapMat;
+}
+void liberaSierpinski(int **matriz, int n){
+    for(int i = 0; i < n; i++){
+        free((*matriz)[i]);
+    }
+    free((*matriz));
 }
--- TapetedeSierpinski.h
+++ TapetedeSierpinski.h
@@ -3,5 +3,6 @@
 
 void tapeteSierpinski(int i, int j, int size, int M);
 int **criarMatriz(n);
+void liberaSierpinski(int **matriz, int n);
 
 #endif
