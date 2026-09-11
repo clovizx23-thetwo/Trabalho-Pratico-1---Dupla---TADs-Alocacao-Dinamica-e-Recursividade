2bf92874b9a898fbd10e111eb5b0819cffb81579
clovizx23-thetwo <soarezclovizz018@gmail.com>
2026-09-11 00:31:27 -0300

    sierpinski criando

--- TapetedeSierpinski.h
+++ TapetedeSierpinski.h
@@ -1,4 +1,6 @@
 #ifndef SIERPINSKI_H
 #define SIERPINSKI_H
 
+void tapeteSierpinski(int i, int j, int size, int M);
+
 #endif
--- sierpinskiMain.c
+++ sierpinskiMain.c
@@ -1,4 +1,14 @@
 #include "TapetedeSierpinski.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+#include <math.h>
+
+int main(){
+    int p, n;
+    printf("Digite o nuemero para ser o expoente da matriz: ");
+    scanf("%d", &p);
+    //calculando os laodos da matriz
+    int n = pow(3, p);
+    printf("\nO valor de n eh:\n", n);
+    return 0;
+}
