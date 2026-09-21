cc963bf0532ecc7d0806141ec16572fe07bbce6a
clovizx23-thetwo <soarezclovizz018@gmail.com>
2026-09-21 11:05:54 -0300

    editando o .c

--- TapetedeSierpinski.c
+++ TapetedeSierpinski.c
@@ -9,21 +9,11 @@ void tapeteSierpinski(int i, int j, int size, int **M){
         return;
     }
 
-    int suBloco = size / 3;
+    //cada quadradinho tem tamanho [size / 3], pq são 9 quadradinhos o tapete total
+    int quadradoInterno = size / 3;
 
-    for(int lin = i + suBloco; lin < i + 2*suBloco; lin++){
-        for(int col = j + suBloco; col < j + 2*suBloco; col++){
-            M[lin][col] = 0;
-        }
-    }
+    
 
-    tapeteSierpinski(i, j, suBloco, M);//
-    tapeteSierpinski(i, j + 2*suBloco, suBloco, M);//
-    tapeteSierpinski(i + suBloco, j, suBloco, M);//
-    tapeteSierpinski(i + suBloco, j + 2*suBloco, suBloco, M);//
-    tapeteSierpinski(i + 2*suBloco, j, suBloco, M);//
-    tapeteSierpinski(i + 2*suBloco, j + suBloco, suBloco, M);//
-    tapeteSierpinski(i + 2*suBloco, j + 2*suBloco, suBloco, M);//
 }
 int **criarMatriz(int n){
     int **tapMat = (int**)malloc(sizeof(int*)*n);
