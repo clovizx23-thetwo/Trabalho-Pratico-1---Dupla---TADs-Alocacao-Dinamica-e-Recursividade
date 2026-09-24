f5e1e813ec59c9bf398a4498767343fcdf2bd538
clovizx23-thetwo <soarezclovizz018@gmail.com>
2026-09-24 10:38:50 -0300

    cores removidas

--- TapetedeSierpinski.c
+++ TapetedeSierpinski.c
@@ -6,47 +6,6 @@
 #include <math.h>
 #include <time.h>
 
-// cores e formato de texto
-#define ANSI_RESET            "\x1b[0m"  // desativa os efeitos anteriores
-#define ANSI_BOLD             "\x1b[1m"  // coloca o texto em negrito
-#define ANSI_COLOR_BLACK      "\x1b[30m"
-#define ANSI_COLOR_RED        "\x1b[31m"
-#define ANSI_COLOR_GREEN      "\x1b[32m"
-#define ANSI_COLOR_YELLOW     "\x1b[33m"
-#define ANSI_COLOR_BLUE       "\x1b[34m"
-#define ANSI_COLOR_MAGENTA    "\x1b[35m"
-#define ANSI_COLOR_CYAN       "\x1b[36m"
-#define ANSI_COLOR_WHITE      "\x1b[37m"
-#define ANSI_BG_COLOR_BLACK   "\x1b[40m"
-#define ANSI_BG_COLOR_RED     "\x1b[41m"
-#define ANSI_BG_COLOR_GREEN   "\x1b[42m"
-#define ANSI_BG_COLOR_YELLOW  "\x1b[43m"
-#define ANSI_BG_COLOR_BLUE    "\x1b[44m"
-#define ANSI_BG_COLOR_MAGENTA "\x1b[45m"
-#define ANSI_BG_COLOR_CYAN    "\x1b[46m"
-#define ANSI_BG_COLOR_WHITE   "\x1b[47m"
-
-// macros para facilitar o uso
-#define BOLD(string)       ANSI_BOLD             string ANSI_RESET
-#define BLACK(string)      ANSI_COLOR_BLACK      string ANSI_RESET
-#define BLUE(string)       ANSI_COLOR_BLUE       string ANSI_RESET
-#define RED(string)        ANSI_COLOR_RED        string ANSI_RESET
-#define GREEN(string)      ANSI_COLOR_GREEN      string ANSI_RESET
-#define YELLOW(string)     ANSI_COLOR_YELLOW     string ANSI_RESET
-#define BLUE(string)       ANSI_COLOR_BLUE       string ANSI_RESET
-#define MAGENTA(string)    ANSI_COLOR_MAGENTA    string ANSI_RESET
-#define CYAN(string)       ANSI_COLOR_CYAN       string ANSI_RESET
-#define WHITE(string)      ANSI_COLOR_WHITE      string ANSI_RESET
-#define BG_BLACK(string)   ANSI_BG_COLOR_BLACK   string ANSI_RESET
-#define BG_BLUE(string)    ANSI_BG_COLOR_BLUE    string ANSI_RESET
-#define BG_RED(string)     ANSI_BG_COLOR_RED     string ANSI_RESET
-#define BG_GREEN(string)   ANSI_BG_COLOR_GREEN   string ANSI_RESET
-#define BG_YELLOW(string)  ANSI_BG_COLOR_YELLOW  string ANSI_RESET
-#define BG_BLUE(string)    ANSI_BG_COLOR_BLUE    string ANSI_RESET
-#define BG_MAGENTA(string) ANSI_BG_COLOR_MAGENTA string ANSI_RESET
-#define BG_CYAN(string)    ANSI_BG_COLOR_CYAN    string ANSI_RESET
-#define BG_WHITE(string)   ANSI_BG_COLOR_WHITE   string ANSI_RESET
-
 void tapeteSierpinski(int i, int j, int size, int **M){
     if(size == 1){
         M[i][j] = 1;
@@ -112,13 +71,7 @@ void mainSierpinski() {
 
         for(int line2 = 0; line2 < ns; line2++){
             for(int col2 = 0; col2 < ns; col2++){
-                
-                if(matN[line2][col2] == 0){
-                    printf(" ");
-                }else{
-                    printf(WHITE("1"));
-                }
-
+                printf("%d", matN[line2][col2]);
                 if(col2 < ns-1)
                     printf(" ");
             }
